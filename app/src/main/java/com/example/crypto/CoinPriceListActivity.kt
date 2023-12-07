cbea6fb4d0321082c6c9837b3ca3a45432085fb9
SobeninAlex <sobeninalex@yandex.ru>
2023-12-07 10:51:08 +0500

    add readme

--- app/src/main/java/com/example/crypto/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/crypto/CoinPriceListActivity.kt
@@ -1,17 +1,11 @@
 package com.example.crypto
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.example.crypto.adapters.CoinInfoAdapter
-import com.example.crypto.databinding.ActivityCoinPriceListBinding
-import com.example.crypto.databinding.ItemCoinInfoBinding
 import com.example.crypto.pojo.CoinPriceInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
